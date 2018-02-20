ac636d16fad13a2c316563e93a4975c691b0ca54
zaczzx <zaczzx@163.com>
2018-02-20 00:14:02 +0000

    ADD CAMGROUNDS

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var app = require("express")();
-
+var bodyParser = require("body-parser");
+var campgrounds = [
+       {name:"AAA", image: "https://lh3.googleusercontent.com/6yuWTbqdM-8--QBZ2PSr3zzCYunnvSjATp2URCFJNxKtGRpBiOI01CiKXTzOIqHOKsqgne8pQdumcSjYvrdV_eMif19LDulQYig7WkMH4ewYTc_9HXSpl_5V9OMJlMIe6C5FnekMiX-YjsFKbitQiQgG-zH-KvhsBJxl1PYNX7rVTkGZpEbS0hxf3X01AuaTNiyGfxRxqwsYOPtmRuVwz2R_lJq9Jg1KxexB45gMkAOPhKS7XyhhRGiCdPIPbULzzQo9iZRlDcLWAsFiKyd0FH-0yF1sETraVIIYD0k5ReN7yI7RCCN8lijjab-e9ua6-7TYhi3HXvRX6ReEux02fSKJQ8NkrSWwsFvghxb82SiE4zanR9JYs-QR78Z4HanWRIA0gEWZAtiDU9Nb_yVcSzYawhGDN2GLU6UfFkX1XAV8LWruBcnB8pVZcBU2RBvja9krCN1jD4MmzqSCeA4qkuDb-RbA7bn9JVXBUr_0z2wgJSfSTN5xju-AIQxJrfZyuW7Jgrk5pp_xRpo8AO6RGRWkT5NSWdtEBQweHxPdHAjfiptFabjevNkJBKa1Q00fAuk-Ph4XMPLioCQrMPdHTUgEdbRmAcAWGOLdG9Y=w938-h704-no"},
+       ];
+       
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
@@ -7,13 +12,19 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-   var campgrounds = [
-       {name:"AAA", image: "https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.momsteam.com%2Ffiles%2Fimages%2FSummer_camp_tents.jpg&imgrefurl=http%3A%2F%2Fwww.momsteam.com%2Fhealth-safety%2Fpreventing-sexual-abuse-at-summer-camp-five-tips-for-parents&docid=aDcqvbDw_y4MVM&tbnid=w3Vbpzz982tWRM%3A&vet=10ahUKEwjjxLX8k7PZAhWLx4MKHUV8AswQMwj9ASgAMAA..i&w=453&h=265&bih=780&biw=1318&q=camp%20images&ved=0ahUKEwjjxLX8k7PZAhWLx4MKHUV8AswQMwj9ASgAMAA&iact=mrc&uact=8"},
-       {name:"BBB", image: "https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.guntherpublications.com%2Fcore%2Fwp-content%2Fuploads%2F2018%2F01%2Fmanali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg&imgrefurl=http%3A%2F%2Fwww.guntherpublications.com%2Ftop-ten-things-never-knew-camp%2F&docid=3ews9SYc9QgjDM&tbnid=vg-uc64o5pJZxM%3A&vet=10ahUKEwjjxLX8k7PZAhWLx4MKHUV8AswQMwiBAigEMAQ..i&w=1440&h=810&bih=780&biw=1318&q=camp%20images&ved=0ahUKEwjjxLX8k7PZAhWLx4MKHUV8AswQMwiBAigEMAQ&iact=mrc&uact=8"},
-       {name:"CCC", image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.asiliaafrica.com%2Fwp-content%2Fuploads%2F2016%2F08%2FNomadic-Camp-guest-tent-new.jpg&imgrefurl=https%3A%2F%2Fwww.asiliaafrica.com%2Feast-africa-safari%2Ftanzania%2Fruaha%2Fkwihala-camp%2F&docid=6ty9LqnQSLm3mM&tbnid=HrLdeisvn0p2rM%3A&vet=10ahUKEwjjxLX8k7PZAhWLx4MKHUV8AswQMwj-ASgBMAE..i&w=436&h=218&bih=780&biw=1318&q=camp%20images&ved=0ahUKEwjjxLX8k7PZAhWLx4MKHUV8AswQMwj-ASgBMAE&iact=mrc&uact=8"}
-       ];
-    
     res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampGround = {name:name, image:image}
+    campgrounds.push(newCampGround);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
