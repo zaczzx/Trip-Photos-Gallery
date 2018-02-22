9287d3cd3e2030c51103e8dd3d1b9442fd17be39
zaczzx <zaczzx@163.com>
2018-02-22 18:13:48 +0000

    Add Authentication

--- app.js
+++ app.js
@@ -2,16 +2,34 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     Camp = require("./models/camp"),
-    seedDB = require("./seed"),
-    Comment = require("./models/comment")
+    Comment = require("./models/comment"),
+    User = require("./models/user"),
+    seedDB = require("./seed");
     
-seedDB();
 mongoose.connect("mongodb://localhost/yelpcamp"); 
+app.set("view engine", "ejs");
+seedDB();
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(require("express-session")({
+   secret: "secret",
+   resave: false,
+   saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/", function(req, res){
    res.render("landing");
@@ -31,7 +49,7 @@ app.post("/camps", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-    var newCamp = {name:name, image:image, description: description}
+    var newCamp = {name:name, image:image, description: description};
     Camp.create(newCamp, function(err, newCreated){
        if(err){
            console.log(err);
@@ -58,7 +76,7 @@ app.get("/camps/:id", function(req, res) {
 });
 
 // ==============
-app.get("/camps/:id/comments/new", function(req, res) {
+app.get("/camps/:id/comments/new", isLoggedIn, function(req, res) {
    Camp.findById(req.params.id, function(err, camp){
       if (err) {
           console.log(err);
@@ -68,7 +86,7 @@ app.get("/camps/:id/comments/new", function(req, res) {
    }); 
 });
 
-app.post("/camps/:id/comments", function(req, res){
+app.post("/camps/:id/comments", isLoggedIn, function(req, res){
    Camp.findById(req.params.id, function(err, camp) {
         if (err) {
             console.log(err);
@@ -87,6 +105,45 @@ app.post("/camps/:id/comments", function(req, res){
    });
 });
 
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/camps");
+        });
+    });
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/camps",
+        failureRedirect: "/login"
+    }), function(req, res) {
+});
+
+app.get("/logout", function(req, res) {
+   req.logout();
+   res.redirect("/camps");
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER WORKS");
 });
