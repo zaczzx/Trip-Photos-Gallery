b4a322a116b1b8bb462ff862aae40c551f71892b
zaczzx <zaczzx@163.com>
2018-02-28 06:38:36 +0000

    Update login & signUp UI and fix registration flash message bug

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get("/", function(req, res){
 
 //sign up form
 router.get("/register", function(req, res) {
-    res.render("register");
+    res.render("register", {page:'register'});
 });
 
 //sign up logic
@@ -18,8 +18,8 @@ router.post("/register", function(req, res) {
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user) {
         if (err) {
-            req.flash("error", err.message);
-            return res.redirect("register");
+            console.log(err);
+            return res.render("register", {error: err.message});
         }
         passport.authenticate("local")(req, res, function(){
             req.flash("success", "Welcome to YelpCamp " + user.username);
@@ -30,7 +30,7 @@ router.post("/register", function(req, res) {
 
 //login form
 router.get("/login", function(req, res) {
-    res.render("login");
+    res.render("login", {page: 'login'});
 });
 
 //login logic
