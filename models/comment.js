37cf0b12ade927b136352b0e096e64939069ea0b
zaczzx <zaczzx@163.com>
2018-03-18 21:41:07 +0000

    add trim false

--- models/comment.js
+++ models/comment.js
@@ -2,6 +2,7 @@ var mongoose = require("mongoose");
 
 var commentSchema = mongoose.Schema({
     text: String,
+    trim: false,
     createdAt: {type: Date, default: Date.now},
     author: {
         id: {
